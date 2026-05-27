971fa03d1a85d4dc5b4a1d31034cc8ee64624810
Jeronimo Soto Villada <jeronimosoto100@gmail.com>
2026-05-27 08:09:32 -0500

    fix: ruta absoluta dontenv en db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: '/var/www/empresas-app/proyecto_colegio_pg_4/.env' });
 
 // Volvemos a MySQL para compatibilidad con phpMyAdmin según solicitud del usuario
 const sequelize = new Sequelize(
